f49914b697e96884b18fbca35b9857f832942eac
zayanhfzh <zayan54562@gmail.com>
2025-09-07 12:57:14 +0700

    fck u 4

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,13 @@
-// api/index.ts
-import 'module-alias/register';         
-import serverless from 'serverless-http';
-import app from '../src/app';
+// api/index.js
+const path = require('path');
 
-export default serverless(app);
+// ⛳️ Map alias "@/..." ke HASIL BUILD: dist/**  (bukan dist/src/**)
+require('module-alias')({
+  '@': path.join(__dirname, '../dist'),
+});
+
+const serverless = require('serverless-http');
+// app.ts -> dist/app.js, jadi require ke ../dist/app
+const app = require('../dist/app').default;
+
+module.exports = serverless(app);
